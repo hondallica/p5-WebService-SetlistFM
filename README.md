47e91e7e548da6c7c70b5bea1149cee26f9d0a5c
Hondallica <hondallica@gmail.com>
2014-09-05 06:18:45 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+WebService::SetlistFM - It's new $module
+
+# SYNOPSIS
+
+    use WebService::SetlistFM;
+
+# DESCRIPTION
+
+WebService::SetlistFM is ...
+
+# LICENSE
+
+Copyright (C) Hondallica.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Hondallica <hondallica@gmail.com>
---
+++ lib/WebService/SetlistFM.pm
@@ -0,0 +1,39 @@
+package WebService::SetlistFM;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WebService::SetlistFM - It's new $module
+
+=head1 SYNOPSIS
+
+    use WebService::SetlistFM;
+
+=head1 DESCRIPTION
+
+WebService::SetlistFM is ...
+
+=head1 LICENSE
+
+Copyright (C) Hondallica.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Hondallica E<lt>hondallica@gmail.comE<gt>
+
+=cut
+
